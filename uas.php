5599ca54fef637d213b8fe061400b603cbac05cb
sayaarisetiawan <ari_22510010@stimata.ac.id>
2023-06-26 20:13:54 +0700

    8

--- uas.php
+++ uas.php
@@ -1,13 +1,22 @@
 <?php 
-$x =1;
-while ($x <=5) {
-    echo "Nomor Urut : $x <br>";
-    $x++;
-}
+$t=date("H");
+
+if ($t < "20") {
+    echo "Have a good day!";
+} else
+    echo "Have a nice day!";
+    echo "<br>";
+    echo "Program ini adalah contoh sederhana dalam bahasa PHP yang mengecek waktu saat ini dan mencetak pesan berdasarkan waktu tersebut.<br>
+
+    - Baris pertama `$t=date(H); mengambil waktu saat ini dalam format jam menggunakan fungsi `date(H)` dan menyimpannya dalam variabel $t.<br>
+    
+    - Baris kedua `if ($t < 20) {` memeriksa apakah waktu saat ini kurang dari 20 (8 malam). Jika kondisi tersebut benar, maka blok kode di dalam kurung kurawal `{}` akan dijalankan.<br>
+    
+    - Baris ketiga `echo Have a good day!;` mencetak pesan Have a good day! jika waktu saat ini kurang dari 20.<br>
+    
+    - Jika kondisi pada baris kedua tidak terpenuhi (waktu saat ini lebih dari atau sama dengan 20), maka program akan melanjutkan ke baris keempat `} else` dan menjalankan blok kode di dalam kurung kurawal `{}` setelahnya.<br>
+    
+    - Baris kelima `echo Have a nice day!;` mencetak pesan Have a nice day! jika waktu saat ini lebih dari atau sama dengan 20.<br>
+    
+    Jadi, program ini akan mencetak pesan Have a good day! jika waktu saat ini kurang dari 20 (8 malam), dan mencetak pesan Have a nice day! jika waktu saat ini lebih dari atau sama dengan 20.";
 ?>
-<!-- program memiliki output
-Nomor Urut : 1
-Nomor Urut : 2
-Nomor Urut : 3
-Nomor Urut : 4
-Nomor Urut : 5 -->
